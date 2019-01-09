2dc762e03b8a1140784b22f7b4857ac05171d026
Roman Schejbal <schejbalroman@gmail.com>
2019-01-09 20:10:22 +0100

    Add url

--- src/App.js
+++ src/App.js
@@ -12,31 +12,30 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <GoogleMap
-          defaultZoom={8}
-          defaultCenter={{ lat: 49.75458, lng: 15.471093 }}
-        >
-          {this.props.prodejci.map(({ lat, lng, ...prodejce }, i) => (
-            <Marker
-              key={i}
-              position={{ lat, lng }}
-              onClick={() => this.setState({ openedMarker: i })}
-            >
-              {this.state.openedMarker === i ? (
-                <InfoWindow
-                  onCloseClick={() => this.setState({ openedMarker: null })}
-                >
-                  <div>
-                    <h4>{prodejce.nazev}</h4>
-                    <p>...</p>
-                  </div>
-                </InfoWindow>
-              ) : null}
-            </Marker>
-          ))}
-        </GoogleMap>
-      </div>
+      <GoogleMap
+        defaultZoom={8}
+        defaultCenter={{ lat: 49.75458, lng: 15.471093 }}
+      >
+        {this.props.prodejci.map(({ lat, lng, url, ...prodejce }, i) => (
+          <Marker
+            key={i}
+            position={{ lat, lng }}
+            onClick={() => this.setState({ openedMarker: i })}
+          >
+            {this.state.openedMarker === i ? (
+              <InfoWindow
+                onCloseClick={() => this.setState({ openedMarker: null })}
+              >
+                <div>
+                  <h4>{prodejce.nazev}</h4>
+                  <p>...</p>
+                  <a href={url}>Podrobné informace</a>
+                </div>
+              </InfoWindow>
+            ) : null}
+          </Marker>
+        ))}
+      </GoogleMap>
     );
   }
 }
