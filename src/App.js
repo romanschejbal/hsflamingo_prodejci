6e018138aa29b8fc9cbd95d6cce4b5a026d6c76e
Roman Schejbal <schejbalroman@gmail.com>
2019-01-09 18:56:36 +0100

    Init HSFlamingo map

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  InfoWindow
+} from "react-google-maps";
 
 class App extends Component {
+  state = { openedMarker: null };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <GoogleMap
+          defaultZoom={8}
+          defaultCenter={{ lat: 49.75458, lng: 15.471093 }}
+        >
+          {this.props.prodejci.map(({ lat, lng, ...prodejce }, i) => (
+            <Marker
+              key={i}
+              position={{ lat, lng }}
+              onClick={() => this.setState({ openedMarker: i })}
+            >
+              {this.state.openedMarker === i ? (
+                <InfoWindow
+                  onCloseClick={() => this.setState({ openedMarker: null })}
+                >
+                  <div>
+                    <h4>{prodejce.nazev}</h4>
+                    <p>Makáááme</p>
+                  </div>
+                </InfoWindow>
+              ) : null}
+            </Marker>
+          ))}
+        </GoogleMap>
       </div>
     );
   }
 }
 
-export default App;
+export default withScriptjs(withGoogleMap(App));
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+// import "./index.css";
+import App from "./App";
+// import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default function renderMap(data, container) {
+  ReactDOM.render(
+    <App
+      googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyB_LaaS3U3CFKlscbug2He-GDAKPstQVds"
+      loadingElement={"Načítám"}
+      containerElement={<div style={{ height: window.innerHeight }} />}
+      mapElement={<div style={{ height: `100%` }} />}
+      prodejci={data}
+    />,
+    container
+  );
+
+  return () => ReactDOM.unmountComponentAtNode(container);
+}
+
+window.renderMap = renderMap;
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
